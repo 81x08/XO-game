a4f074293bd0b2e19fc8cfaaae21f5ce789e88e0
81x08 <nixon133@mail.ru>
2018-06-07 21:15:14 +0500

    CurrentMoveController implementation

---
+++ src/com/game/xo/controllers/CurrentMoveController.java
@@ -0,0 +1,42 @@
+package com.game.xo.controllers;
+
+import com.game.xo.model.Field;
+import com.game.xo.model.Figure;
+import com.game.xo.model.exception.InvalidPointException;
+
+import java.awt.*;
+
+public class CurrentMoveController {
+
+    public Figure currentMove(final Field field) {
+        int countFigure = 0;
+
+        for (int x = 0; x < field.getSize(); x++) {
+            countFigure += countFiguresInTheRow(field, x);
+        }
+
+        if (countFigure == field.getSize() * field.getSize())
+            return null;
+
+        if (countFigure % 2 == 0)
+            return Figure.X;
+
+        return Figure.O;
+    }
+
+    private int countFiguresInTheRow(final Field field, final int row) {
+        int countFigure = 0;
+
+        for (int x = 0; x < field.getSize(); x++) {
+            try {
+                if (field.getFigure(new Point(x, row)) != null)
+                    countFigure++;
+            } catch (final InvalidPointException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return countFigure;
+    }
+
+}
